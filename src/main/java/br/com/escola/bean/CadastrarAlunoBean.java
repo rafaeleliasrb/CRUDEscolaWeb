8d2f47829c8ee015c5edbcb44b1b2ac39a3e66a1
TCE\rafael.elias <rafael.elias@tce.ce.gov.br>
2018-05-22 14:06:35 -0300

    Fazendo a ligação, binding, com o ManagedBean

---
+++ src/main/java/br/com/escola/bean/CadastrarAlunoBean.java
@@ -0,0 +1,11 @@
+package br.com.escola.bean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CadastrarAlunoBean {
+
+	public void salvar() {
+		System.out.println("Salvando usuário...");
+	}
+}
