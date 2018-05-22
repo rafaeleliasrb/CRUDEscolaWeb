c76147701b36e72fdc5936cf6c0a30a7cf129823
TCE\rafael.elias <rafael.elias@tce.ce.gov.br>
2018-05-22 15:22:20 -0300

    Salvando um aluno e limpando a tela

--- src/main/java/br/com/escola/bean/CadastrarAlunoBean.java
+++ src/main/java/br/com/escola/bean/CadastrarAlunoBean.java
@@ -3,15 +3,19 @@ package br.com.escola.bean;
 import javax.faces.bean.ManagedBean;
 
 import br.com.escola.model.domain.Aluno;
+import br.com.escola.model.service.AlunoService;
 
 @ManagedBean
 public class CadastrarAlunoBean {
 
+	private AlunoService alunoService = new AlunoService();
+	
 	private Aluno aluno = new Aluno();
 	
 	public void salvar() {
 		System.out.println("Salvando aluno ...");
-		System.out.println(aluno.toString());
+		alunoService.salvar(aluno);
+		aluno = new Aluno();
 	}
 
 	public Aluno getAluno() {
